0583ba1e13fc39410b81c6b1892354e3c851818d
SOFTLY <116372539+softly2000@users.noreply.github.com>
2022-10-27 18:40:59 +0300

    fix_knu_cpp

---
+++ 13zad ictino/13zadiction.cpp
@@ -0,0 +1,53 @@
+int main()
+{
+          /*1vari*/
+          
+    /*string data[5];
+    cout << "Введите данные о себе!" << endl;
+    cout << "Фамилию - ";
+    cin >> data[0];
+    cout << "Имя - ";
+    cin >> data[1];
+    cout << "Год рождения - ";
+    cin >> data[2];
+    cout << "Месяц рождения - ";
+    cin >> data[3];
+    cout << "День рождения - ";
+    cin >> data[4];
+    int biday = stoi(data[3]);
+    int days = stoi(data[4]);
+    int dataday = biday + days;
+    int day;
+    cout << "Введите текущий месяц - ";
+    cin >> day;
+    cout << "Введите текущий день - ";
+    cin >> day;
+    if(dataday < day)cout << "Для " << data[0] << " " << data[1] << " день рождения прошел!";
+    else cout << "Для " << data[0] << " " << data[1] << " день рождения будет позже!";*/
+    
+    
+             /*2vari*/
+    /*string data[5];
+    cout << "Введите данные о себе!" << endl;
+    cout << "Фамилию - ";
+    cin >> data[0];
+    cout << "Имя - ";
+    cin >> data[1];
+    cout << "Год рождения - ";
+    cin >> data[2];
+    cout << "Месяц рождения - ";
+    cin >> data[3];
+    cout << "День рождения - ";
+    cin >> data[4];
+    int biday = stoi(data[3,4]);
+    int day;
+    cout << "Введите текущий месяц - ";
+    cin >> day;
+    cout << "Введите текущий день - ";
+    cin >> day;
+    if(biday < day)cout << "Для " << data[0] << " " << data[1] << " день рождения прошел!";
+    else cout << "Для " << data[0] << " " << data[1] << " день рождения будет позже!";*/
+
+    return 0;
+}
+
